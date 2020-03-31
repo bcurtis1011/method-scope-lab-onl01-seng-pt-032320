e96a0c86a1505ce2821d7faef49ba0a9b4f5edfc
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-03-31 18:45:49 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -5,6 +5,7 @@ def rescue_princess_peach(status)
   puts "Hooray! Mario has #{status} Princess Peach."
 end
 
+rescue_princess_peach(failed)
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
